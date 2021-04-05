076e1e819eace4dc79b91e8763e03c9056753f07
Philipp <37447039+psypdt@users.noreply.github.com>
2021-04-05 19:52:32 +0200

    add scheduler

---
+++ trc/util/ScheduleThread.py
@@ -0,0 +1,13 @@
+import threading
+import time
+import schedule
+
+
+class ScheduleThread(threading.Thread):
+    def __init__(self, *pargs, **kwargs):
+        super().__init__(*pargs, daemon=True, name="scheduler", **kwargs)
+
+    def run(self):
+        while True:
+            schedule.run_pending()
+            time.sleep(schedule.idle_seconds())
