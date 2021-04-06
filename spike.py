7e8571b6387a912e30006089949fb97e41584be5
Alexander <aljaeger@student.ethz.ch>
2021-04-06 18:31:26 +0200

    added interactive plot to spike.py

--- spike.py
+++ spike.py
@@ -3,16 +3,22 @@ import matplotlib.pyplot as plt
 import os
 from coinbase.wallet.client import Client
 import time
+from multiprocessing import Process
 
 current_path = os.path.abspath(os.path.dirname(__file__))
 API_file = open(current_path+"/API_key.txt")
 
 key = API_file.readline().strip("\n")
 secret = API_file.readline().strip("\n")
-
+key = key
 client = Client(key, secret)
 
 currencies = ["BTC", "EOS", "ETH", "ZRX", "XLM", "OMG", "XTZ", "BCH", "LTC", "GRT", "FIL", "ANKR", "COMP"]
+
+# create dictionary containing colors for each coin (for plots)
+colors = ["orange", "black", "blue", "grey", "blue", "purple", "green", "darkblue", "red", "lightblue", "pink",
+          "darkred", "darkgreen"]
+colors = {coin: colors[i] for i, coin in enumerate(currencies)}
 
 
 def get_historical(coin: str, period: str = "day") -> (list, list):
@@ -33,7 +39,7 @@ def get_historical(coin: str, period: str = "day") -> (list, list):
     return times, prices
 
 
-def update_graphs(currencies : list) -> None:
+def update_graphs(currencies: list) -> None:
     """
     saves graphs of prices for each coin in currencies list with respect to CHF.
     :param currencies: list containing coin strings.
@@ -50,7 +56,80 @@ def update_graphs(currencies : list) -> None:
         plt.close()
 
 
-def get_price_change(coin : str, period: str = "day") -> float:
+def normalised_price_graph(currencies: list, period: str = "day", filename: str = "trend_graph.png",
+                           clear_plot: bool = True, significance_threshold: float = 0.01) -> None:
+    """
+    saves a plot of the most significant changing currencies on a normalized graph. By default the pyplot is cleared,
+    but this can be changed using clear_plot.
+    :param currencies: the list of currencies to be graphed.
+    :param period: the time period to be graphed.
+    :param filename: the filename of the output image.
+    :param clear_plot: specifies whether the plot should be cleared. (default = True)
+    :param significance_threshold: defines at which point a change becomes significant
+    :return: None
+    """
+    plt.clf()   # clear graph
+    plt.grid()  # plot grid
+
+    prices_list = {}
+    times_list = {}
+    percentage_change = {}
+
+    for coin in currencies:
+        times, prices = get_historical(coin, period)
+        prices_list.update({coin: prices})
+        times_list.update({coin: times})
+        percentage_change.update({coin: (prices[-1] - prices[0]) / prices[0]})
+
+    sorted_currencies = (sorted(percentage_change)[::-1])          # sorted in order of decreasing percentage change
+    for coin in sorted_currencies[:3] + sorted_currencies[-4:]:    # include first 3 most increasing/decreasing coins.
+        if np.abs(percentage_change[coin]) > significance_threshold:  # include only significant changes in the graph
+            percentage_change_graph = 100*(np.array(prices_list[coin]) / prices_list[coin][0] - 1)
+            plt.plot(np.linspace(-24, 0, len(prices_list[coin])), percentage_change_graph, label=coin,
+                     color = colors[coin])
+
+            # add coin labels to plot
+            sign = np.sign(percentage_change[coin])
+            if sign >= 0:
+                sign = "+"
+            else:
+                sign = "-"
+            plt.text(1.5, percentage_change_graph[-1], sign + "%2.0f%%" % (np.abs(percentage_change[coin]*100)) + " "
+                     + coin, color = colors[coin],fontsize=7)
+            if not clear_plot:
+                plt.pause(0.001)  # the pause statements are required such that an interactive graph updates properly.
+
+    # plot labels etc.
+    plt.legend()
+    plt.xlabel("Time")
+    plt.ylabel("Price Change (%)")
+
+    if not clear_plot:
+        plt.pause(0.1)  # the pause statements are required such that an interactive graph updates properly.
+
+    plt.savefig(current_path + "/graphs/" + filename)
+
+    if clear_plot:
+        plt.clf()
+
+
+def thread_price_graph(currencies: list, period: str = "day", filename: str = "trend_graph.png", delay: int = 5*60)\
+        -> None:
+    """
+    displays an interactive price plot which is updated on a periodic basis.
+    :param currencies: the currencies to be considered for plotting.
+    :param period: the time period to be graphed.
+    :param filename: the filename of the output image.
+    :param delay: specifies the delay between plot updates.
+    :return: None
+    """
+    plt.ion()   # enable interactive mode (allows for live updating of the plot)
+    while True:
+        normalised_price_graph(currencies, period, filename, clear_plot = False)
+        plt.pause(delay)
+
+
+def get_price_change(coin: str, period: str = "day") -> float:
     """
     calculates percentage change for a coin over the past hour.
     :param coin: the coin for which the percentage is calculated
@@ -59,23 +138,50 @@ def get_price_change(coin : str, period: str = "day") -> float:
     """
     times, prices = get_historical(coin)
     diff = prices[-1] - prices[0]
-    relative_diff =  diff / prices[0]
-    return float(relative_diff)
+    relative_diff = diff / prices[0]
+    return float(relative_diff*100)
 
 
 if __name__ == '__main__':
+    # run interactive plot in a separate process
+    process = Process(target = thread_price_graph, args = (currencies, ))
+    process.start()
+
     day_threshold = 10
     hour_threshold = 10
+    delay = 5*60
+
+    # dictionary containing the previously notified price percentage change, which is used to prevent repeated
+    # notifications.
+    notified = {coin: 0 for coin in currencies}
+    notification_threshold = 5  # threshold for sending a new notification (%)
+
     while True:
-        changes = []
+        changes = []                # list containing the percentage price change,
+        new_notifications = False   # specifies whether a notification is new or not.
+
         for coin in currencies:
             change = get_price_change(coin)
             changes.append(change)
-            #print(coin + " : " + "%.2f" % (change*100))
 
-            if change*100 > day_threshold*0:
-                print(" ↑ " + coin + " increased by " + "%.2f"%(change*100) + " %")
-            elif change*100 < -day_threshold*0:
-                print(" ↓ "+coin + " decreased by " + "%.2f"%(-change*100) + " %")
-        print("\n")
-        time.sleep(5*60)
+            # notify if the price changed significantly today, if this increased by more than the notification_threshold
+            # from the previous notification.
+            if change > day_threshold and change - notified[coin] > notification_threshold:
+                print(" ↑ " + coin + " increased by " + "%.2f" % change + " %")
+                notified[coin] = change
+                new_notifications = True
+            elif change < -day_threshold and change - notified[coin] < notification_threshold:
+                print(" ↓ "+coin + " decreased by " + "%.2f" % (-change) + " %")
+                notified[coin] = change
+                new_notifications = True
+
+        if not new_notifications:
+            print("No significant changes.")
+
+        current_time = time.strftime("%H:%M:%S", time.localtime())
+        print("checked at " + str(current_time),end="\n\n")
+
+        time.sleep(delay)
+
+    # currently won't be executed, but I left it in case we use break statements later.
+    process.kill()
