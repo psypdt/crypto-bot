c2c81354458506da8f02b16e3b71f8168a26fa44
Philipp <psypdt@nottingham.ac.uk>
2021-04-08 00:30:28 +0200

    Separate main loop into function

--- spike.py
+++ spike.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 import time
 from utils.coinbase_utils import CoinbaseAPI as wpr
+from typing import List
 
 if os.name == 'nt':
     import winsound
@@ -12,6 +13,46 @@ CURRENCIES = ["BTC", "EOS", "ETH", "ZRX", "XLM", "OMG", "XTZ", "BCH", "LTC", "GR
 def beep(freq, duration):
     if os.name == 'nt':
         winsound.Beep(freq, duration)
+
+
+def get_spike_alerts(is_console=False) -> List[str]:
+    """
+    Queries the coinbase API to get updates on significant changes in currencies
+    :param is_console: Flag set for console usage vs Telegram Bot usage to get time readout
+    :return: A list of strings which pertain to a currency
+    """
+    increasing_list = list()
+    decreasing_list = list()
+    changes = []  # list containing the percentage price change,
+
+    for coin in CURRENCIES:
+        change = wrapper.get_price_change(coin)
+        changes.append(change)
+
+        # notify if the price changed significantly today, if this increased by more than the notification_threshold
+        # from the previous notification.
+        if change > day_threshold and change - notified[coin] > notification_threshold:
+            increasing_list.append(" ↑ " + coin + " increased by " + "%.2f" % change + " %")
+            notified[coin] = change
+
+        elif change < -day_threshold and change - notified[coin] < -notification_threshold:
+            decreasing_list.append(" ↓ " + coin + " decreased by " + "%.2f" % (-change) + " %")
+            notified[coin] = change
+
+        # if np.abs(change) < day_threshold / 2 < np.abs(notified[coin]):
+        #     if notified[coin] > 0:
+        #         messages.append("Spike has ended for " + coin + ".")
+        #     elif notified[coin] < 0:
+        #         messages.append("Dip has ended for " + coin + ".")
+        #     notified[coin] = 0
+
+    message = increasing_list + decreasing_list
+    if is_console:
+        current_time = time.strftime("%H:%M:%S", time.localtime())
+        time_stamp = "checked at " + str(current_time) + "\n\n"
+        message.append(time_stamp)
+
+    return message
 
 
 if __name__ == '__main__':
@@ -29,39 +70,7 @@ if __name__ == '__main__':
     notification_threshold = 5  # threshold for sending a new notification (%)
 
     while True:
-        changes = []                # list containing the percentage price change,
-        new_notifications = False   # specifies whether a notification is new or not.
-
-        balances = wrapper.get_account_balance(CURRENCIES)
-
-        for coin in CURRENCIES:
-            change = wrapper.get_price_change(coin)
-            changes.append(change)
-
-            # notify if the price changed significantly today, if this increased by more than the notification_threshold
-            # from the previous notification.
-            if change > day_threshold and change - notified[coin] > notification_threshold:
-                print(" ↑ " + coin + " increased by " + "%.2f" % change + " %")
-                beep(880, 500)
-                notified[coin] = change
-                new_notifications = True
-            elif change < -day_threshold and change - notified[coin] < -notification_threshold:
-                print(" ↓ "+coin + " decreased by " + "%.2f" % (-change) + " %")
-                beep(220, 500)
-                notified[coin] = change
-                new_notifications = True
-            if np.abs(change) < day_threshold/2 < np.abs(notified[coin]):
-                beep(440, 1000)
-                if notified[coin] > 0:
-                    print("Spike has ended for " + coin + ".")
-                elif notified[coin] < 0:
-                    print("Dip has ended for " + coin + ".")
-                notified[coin] = 0
-
-        if not new_notifications:
-            print("No significant changes.")
-
-        current_time = time.strftime("%H:%M:%S", time.localtime())
-        print("checked at " + str(current_time), end="\n\n")
-
+        results = get_spike_alerts(is_console=True)
+        for change in results:
+            print(change)
         time.sleep(delay)
