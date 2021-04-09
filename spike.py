786bf4658c817532f07fbf3f058a84b6f51b1a18
Alexander <aljaeger@student.ethz.ch>
2021-04-10 01:21:48 +0200

    Changed how messages are generated (incomplete)

--- spike.py
+++ spike.py
@@ -1,25 +1,75 @@
 from utils.coinbase_utils import CoinbaseAPI as cbapi
 import utils.coinbase_utils.GlobalStatics as statics
+import numpy as np
 import time
 import os
 
 
 class Spike:
-    def __init__(self, currencies: list, notification_threshold: float, day_threshold: float,
-                 coinbase_api: cbapi.CoinbaseAPI):
+    def __init__(self, currencies: list, coinbase_api: cbapi.CoinbaseAPI, notification_threshold: float,
+                 day_threshold: float = 0, week_threshold: float = 0.):
         """
         :param currencies: List of crypto currency identifiers (BTC, XRP, etc.)
         :param notification_threshold: Amount in (%) needed for another notification to be sent for a coin
-        :param day_threshold: Minimum (%) change over an entire day needed to send notification
+        :param day_threshold: Minimum (%) change over an entire day needed to trigger a notification
+        :param week_threshold: Minimum (%) change over a week needed to trigger a notification
         :param coinbase_api: Coinbase API object to fetch data for crypto currencies
         """
         self.currencies = currencies
         self.notification_threshold = notification_threshold  # threshold for sending a new notification (%)
         self.day_threshold = day_threshold
+        self.week_threshold = week_threshold
         self.coinbase_api = coinbase_api
 
         # dictionary containing the previously notified price percentage change, used to prevent repeated notifications.
         self.notified = {coin: 0 for coin in statics.CURRENCIES}
+
+    def __generate_alert_string(self, coin: str, percentage_change: float, period: str):
+        alert_string = ""
+        if percentage_change >= 0:
+            if period == "week":
+                alert_string = "+" + "%2.2f" % percentage_change + " " + coin + " this week"
+
+        return alert_string
+
+    def __generate_alert(self, coin: str, period: str) -> (float, str):
+        """
+        Generates a tuple containing the percentage change and alert message.
+
+        :param coin: The coin for which the alert message is to be generated.
+        :param period: The period for which the percentage change should be considered
+        :return: A tuple containing the percentage change and alert message.
+        """
+        alert_tuple = None
+        percentage_change = self.coinbase_api.get_price_change(coin, period=period)
+        threshold = 10
+
+        if period == "week":
+            threshold = self.week_threshold
+        elif period == "day":
+            threshold = self.day_threshold
+
+        # notify if the price changed significantly today, if this increased by more than the notification_threshold
+        # from the previous notification.
+        if np.abs(percentage_change) > threshold and np.abs(percentage_change - self.notified[coin]) > \
+            self.notification_threshold:
+            alert_tuple = (percentage_change, self.__generate_alert_string(coin, percentage_change, period))
+
+        # if percentage_change > threshold and percentage_change - self.notified[coin] > \
+        #         self.notification_threshold:
+        #     alert_tuple = (percentage_change,
+        #                     "↑ " + coin + " increased by " + "%.2f" % percentage_change + " %")
+        #
+        #     self.notified[coin] = percentage_change
+        #
+        # elif percentage_change < -threshold and percentage_change - self.notified[coin] < \
+        #         -self.notification_threshold:
+        #     alert_tuple = (percentage_change,
+        #              "↓ " + coin + " decreased by " + "%.2f" % (-percentage_change) + " %")
+        #
+        #    self.notified[coin] = percentage_change
+
+        return alert_tuple
 
     def get_spike_alerts(self, is_console=False) -> [(float, str)]:
         """
@@ -29,32 +79,11 @@ class Spike:
         :return: A list of tuples where the key is percentage change & value is the entire message string
         """
         message = list()
-        changes = []  # list containing the percentage price change,
 
         for coin in statics.CURRENCIES:
-            percentage_change = self.coinbase_api.get_price_change(coin)
-            changes.append(percentage_change)
-
-            # notify if the price changed significantly today, if this increased by more than the notification_threshold
-            # from the previous notification.
-            if percentage_change > self.day_threshold and percentage_change - self.notified[coin] > \
-                    self.notification_threshold:
-                message.append((percentage_change, "↑ " + coin + " increased by " +
-                                        "%.2f" % percentage_change + " %"))
-                self.notified[coin] = percentage_change
-
-            elif percentage_change < -self.day_threshold and percentage_change - self.notified[coin] < \
-                    -self.notification_threshold:
-                message.append((percentage_change, "↓ " + coin + " decreased by " +
-                                        "%.2f" % (-percentage_change) + " %"))
-                self.notified[coin] = percentage_change
-
-            # if np.abs(change) < day_threshold / 2 < np.abs(notified[coin]):
-            #     if notified[coin] > 0:
-            #         messages.append("Spike has ended for " + coin + ".")
-            #     elif notified[coin] < 0:
-            #         messages.append("Dip has ended for " + coin + ".")
-            #     notified[coin] = 0
+            alert_tuple = self.__generate_alert(coin, period = "week")
+            if alert_tuple:
+                message.append(alert_tuple)
 
         message.sort(key=lambda tup: tup[0], reverse=True)
         message = [value for key, value in message]
@@ -69,9 +98,10 @@ class Spike:
 
 if __name__ == '__main__':
     current_path = os.path.abspath(os.path.dirname(__file__))
-    api_file = str(current_path + "/API_key.json")
+    api_file = str(current_path + "/credentials/API_key.json")
     cb_api = cbapi.CoinbaseAPI(api_file)
-    spike = Spike(statics.CURRENCIES, day_threshold=10, notification_threshold=5, coinbase_api=cb_api)
+    spike = Spike(statics.CURRENCIES, coinbase_api=cb_api, day_threshold=1, week_threshold=1,
+                  notification_threshold=5)
 
     delay = 5*60
 
