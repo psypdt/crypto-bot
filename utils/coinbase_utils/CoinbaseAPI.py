e861c645e41343de35fdced78f4bdb0e1be06054
Philipp <psypdt@nottingham.ac.uk>
2021-04-12 00:10:21 +0200

    Add method to give exchange rate at a given timestamp

--- utils/coinbase_utils/CoinbaseAPI.py
+++ utils/coinbase_utils/CoinbaseAPI.py
@@ -1,5 +1,7 @@
 from coinbase.wallet.client import Client
+from coinbase.wallet.model import Money
 import json
+import os
 
 
 class CoinbaseAPI:
@@ -56,12 +58,58 @@ class CoinbaseAPI:
         relative_diff = diff / prices[0]
         return float(relative_diff*100)
 
+    def get_account_balance(self, currencies: list) -> dict:
+        """
+        Retrieves the balance of a users account
 
-    def get_account_balance(self, currencies: list):
+        :param currencies: A list of crypto codes (BTC, XLM, etc.)
+        :return: Dictionary of currency, balance pairs
+        """
         accounts = []
         for coin in currencies:
             accounts.append(self.client.get_account(coin))
 
         balances = {account["balance"]["currency"]: float(account["balance"]["amount"]) for account in accounts}
         return balances
+
+    def get_transaction_history(self, coin) -> [(float, str)]:
+        """
+        Gets all transactions that have been carried out with the given input currency.
+
+        NOTE: When currencies are received the amount is positive, if a coin is traded for another one (BTC -> ZRX)
+        then the BTC transaction will be negative
+
+        :param coin: The code for a currency whose historical transactions will be queried (BTC, ZRX, etc.)
+        :return: A list of coin_amount, timestamp tuples
+        """
+        transaction_list = self.client.get_transactions(coin).get('data')
+        num_coins_traded = [float(trans["amount"].get("amount")) for trans in transaction_list]
+        timestamps = [trans["created_at"] for trans in transaction_list]
+
+        return list(zip(num_coins_traded, timestamps))
+
+    def get_historic_exchange_rate(self, from_currency: str, to_currency: str, timestamp: str) -> float:
+        """
+        Used to get an exchange rate between 2 currencies at a given timestamp
+
+        NOTE: To get the price of 1 BTC in CHF we would do get_historic_exchange_rate(BTC, CHF, time)
+
+        :param from_currency: The currency which we want to convert
+        :param to_currency: The currency which will express from_currency
+        :param timestamp: The timestamp in the format YYYY-MM-DDTHH:MM:SSZ where T & Z must be included
+        :return: The value of 1 from_currency expressed as to_currency at a given timestamp
+        """
+        rates = self.client.get_exchange_rates(currency=from_currency, date=timestamp)["rates"]
+        return float(rates[to_currency])
+
+
+if __name__ == '__main__':
+    current_path = os.path.abspath(os.path.dirname(__file__))
+    current_path = "/".join(current_path.split("/")[:-2])
+    file_path = current_path + "/credentials/API_key.json"
+
+    api = CoinbaseAPI(file_path)
+    ret = api.get_transaction_history("BTC")
+    print(api.get_historic_exchange_rate("BTC", "CHF", ret[0][1]))
+
 
